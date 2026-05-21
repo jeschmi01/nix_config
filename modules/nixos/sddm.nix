69ac43dc0bd06a486e09d4b12111a29d63124ba4
Jennifer Schmidt <schmidtjennifer01@gmail.com>
2026-05-21 17:48:12 +0200

    changed sddm theme

--- modules/nixos/sddm.nix
+++ modules/nixos/sddm.nix
@@ -1,19 +1,28 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 
+let
+  custom-astronaut = pkgs.sddm-astronaut.override {
+    embeddedTheme = "pixel_sakura";
+  };
+in
 {
   services.displayManager.sddm = {
     enable = true;
     package = pkgs.kdePackages.sddm;
-    wayland.enable = lib.mkForce true;
-
+    wayland.enable = false;
     theme = "sddm-astronaut-theme";
-
-    extraPackages = [
-      pkgs.sddm-astronaut
-    ];
+    extraPackages = [ custom-astronaut ];
   };
 
-  environment.systemPackages = [
-    pkgs.sddm-astronaut
-  ];
+  services.xserver.enable = true;
+
+  services.xserver.xkb = {
+    layout = "de";
+    variant = "";
+  };
+
+  i18n.defaultLocale = "de_DE.UTF-8";
+  console.keyMap = "de";
+
+  environment.systemPackages = [ custom-astronaut ];
 }
